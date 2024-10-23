e3de6b5dfecc369cda1eecbb550f753d4a9ae72f
Endry <endrybwolf@gmail.com>
2024-10-23 18:36:05 +0200

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+
+- View Live: [Click Here](https://ndree369.github.io/question/)
+
+
---
+++ js/script.js
@@ -0,0 +1,154 @@
+const answers_no = {
+    english: [
+        "No",
+        "Are you sure?",
+        "Are you really sure??",
+        "Are you really realy sure???",
+        "Think again?",
+        "Don't believe in second chances?",
+        "Why are you being so cold?",
+        "Maybe we can talk about it?",
+        "I am not going to ask again!",
+        "Ok now this is hurting my feelings!",
+        "You are now just being mean!",
+        "Why are you doing this to me?",
+        "Please give me a chance!",
+        "I am begging you to stop!",
+        "Ok, Let's just start over.."
+    ],
+    french: [
+        "Non",
+        "Tu es sûr ?",
+        "Tu es vraiment sûr ??",
+        "Tu es vraiment vraiment sûr ???",
+        "Réfléchis encore?",
+        "Tu ne crois pas aux deuxièmes chances ?",
+        "Pourquoi tu es si froid?",
+        "Peut-être, on peut en parler ?",
+        "Je ne vais pas demander encore une fois!",
+        "D'accord, maintenant ca me fait mal!",
+        "Tu es juste méchant!",
+        "Pourquoi tu me fais ça?",
+        "Donnez-moi une chance plz!",
+        "Je te supplie d'arrêter!",
+        "D'accord, recommençons.."
+    ],
+    german: [
+        "Nein",
+        "Bist du sicher?",
+        "Bist du dir wirklich sicher??",
+        "Bist du dir wirklich, wirklich sicher?",
+        "Überleg es noch mal?",
+        "Glaubst du nicht an zweite Chancen?",
+        "Warum bist du so kalt?",
+        "Vielleicht können wir darüber reden?",
+        "Ich werde nicht noch einmal fragen!",
+        "Ok, das verletzt meine Gefühle!",
+        "Du bist jetzt einfach nur gemein!",
+        "Warum tust du mir das an?",
+        "Bitte gib mir eine Chance!",
+        "Ich flehe dich an, hör auf!",
+        "Ok, fangen wir einfach von vorne an..."
+    ]
+};
+
+answers_yes = {
+    "english": "Yes",
+    "french": "Oui",
+    "german": "Ja"
+}
+
+let language = "english"; // Default language is English
+const no_button = document.getElementById('no-button');
+const yes_button = document.getElementById('yes-button');
+let i = 1;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+    // Change banner source
+    let banner = document.getElementById('banner');
+    if (clicks === 0) {
+        banner.src = "images/no.gif";
+        refreshBanner();
+    }
+    clicks++;
+    // increase button height and width gradually to 250px
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answers_no[language].length;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answers_no[language][i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answers_no[language][i]);
+        i = 1;
+        no_button.innerHTML = answers_no[language][0];
+        yes_button.innerHTML = answers_yes[language];
+        yes_button.style.height = "50px";
+        yes_button.style.width = "50px";
+        size = 50;
+    }
+});
+
+yes_button.addEventListener('click', () => {
+    // change banner gif path
+    let banner = document.getElementById('banner');
+    banner.src = "images/yes.gif";
+    refreshBanner();
+    // hide buttons div
+    let buttons = document.getElementsByClassName('buttons')[0];
+    buttons.style.display = "none";
+    // show message div
+    let message = document.getElementsByClassName('message')[0];
+    message.style.display = "block";
+});
+
+function refreshBanner() {
+    // Reload banner gif to force load  
+    let banner = document.getElementById('banner');
+    let src = banner.src;
+    banner.src = '';
+    banner.src = src;
+}
+
+function changeLanguage() {
+    const selectElement = document.getElementById("language-select");
+    const selectedLanguage = selectElement.value;
+    language = selectedLanguage;
+
+    // Update question heading
+    const questionHeading = document.getElementById("question-heading");
+    if (language === "french") {
+        questionHeading.textContent = "Seras-tu mon amour pour toujours?";
+    } else if (language === "german"){
+              questionHeading.textContent = "Wirst Du für immer meine Liebe sein?";
+    } else {
+              questionHeading.textContent = "Will you be my Love forever?";
+    }
+
+
+    // Reset yes button text
+    yes_button.innerHTML = answers_yes[language];
+
+    // Reset button text to first in the new language
+    if (clicks === 0) {
+        no_button.innerHTML = answers_no[language][0];
+    } else {
+        no_button.innerHTML = answers_no[language][clicks];
+    }
+
+    // Update success message
+    const successMessage = document.getElementById("success-message");
+    if (language === "french") {
+        successMessage.textContent = "Yepppie 💘, Je t'aime ma chérie 😘";
+    } else if (language === "german") {
+              successMessage.textContent = "Jaaaa 💘, Ich liebe Dich mein Liebling 😘";
+    } else {
+        successMessage.textContent = "Yepppie 💘, I Love You my darling 😘";
+    }
+}
